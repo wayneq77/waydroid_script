ef90e33b626c7a2d1044600328aab075dbbbdd4e
wayneq77 <wayneq77@gmail.com>
2026-05-27 10:30:23 +0800

    Save latest changes

--- stuff/magisk.py
+++ stuff/magisk.py
@@ -10,7 +10,7 @@ from tools import container
 class Magisk(General):
     id = "magisk delta"
     partition = "system"
-    dl_link = "https://github.com/mistrmochov/magiskdeltaorig/raw/main/app-release.apk"
+    dl_link = "https://github.com/topjohnwu/Magisk/releases/download/v30.7/Magisk-v30.7.apk"
     dl_file_name = "magisk.apk"
     extract_to = "/tmp/magisk_unpack"
     magisk_dir = os.path.join(partition, "etc", "init", "magisk")
